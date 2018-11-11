6fe0c2db02f83eeffd57c0fad6b9873f67a2d393
avkosme <rus.andrew@gmail.com>
2018-11-11 10:06:10 +0300

    Refactoring page view

--- web/page/views.py
+++ web/page/views.py
@@ -1,4 +1,3 @@
-import json
 from django.views import View
 from django.http import HttpResponse
 from django.template import loader
@@ -8,7 +7,9 @@ from page.models import Page as PageModel
 class Page(View):
     @staticmethod
     def get(request, **kwargs):
-        page = PageModel.page_public.filter(slug=kwargs.get('slug')).get()
-        template = loader.get_template(
-            page.template if page.template else 'index.html')
-        return HttpResponse(template.render({'page': page, 'content': page.content_set.get()}, request))
+        try:
+            page = PageModel.page_public.filter(slug=kwargs.get('slug')).get()
+        except PageModel.DoesNotExist:
+            page = PageModel()
+        template = loader.get_template(page.template if page.template else 'index.html')
+        return HttpResponse(template.render({'page': page}, request))
